35d4fb6c7af03b6cec8f87a4f32731ddaa23b8a6
Mukul Palol <mukul.palol@intimetec.com>
2023-03-28 14:21:27 +0530

    DevOps

---
+++ FinalDevOps/FinalDevOps/FinalDevOpsDbContext.cs
@@ -0,0 +1,13 @@
+﻿using FinalDevOps.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalDevOps
+{
+    public class FinalDevOpsDbContext : DbContext
+    {
+        public FinalDevOpsDbContext(DbContextOptions<FinalDevOpsDbContext> options) : base(options)
+        {            
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ FinalDevOps/FinalDevOps/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinalDevOps.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public decimal Salary { get; set; }
+        [Required]
+        public string Department { get; set; }
+    }
+}
